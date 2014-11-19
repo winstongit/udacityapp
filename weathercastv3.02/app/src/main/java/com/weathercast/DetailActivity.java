775f06eb95834242b79b80b6bd9145cd58a86ed9
winstongit <winstongit@gmail.com>
2014-11-19 11:28:01 +0800

    get location from shared prefs

--- weathercastv3.02/app/src/main/java/com/weathercast/DetailActivity.java
+++ weathercastv3.02/app/src/main/java/com/weathercast/DetailActivity.java
@@ -1,5 +1,6 @@
 package com.weathercast;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -40,6 +41,7 @@ public class DetailActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
         return super.onOptionsItemSelected(item);
--- weathercastv3.02/app/src/main/java/com/weathercast/ForecastFragment.java
+++ weathercastv3.02/app/src/main/java/com/weathercast/ForecastFragment.java
@@ -5,9 +5,11 @@ package com.weathercast;
  */
 
 import android.app.Fragment;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -63,7 +65,10 @@ public class ForecastFragment extends Fragment {
         int id = item.getItemId();
         if (id == R.id.action_refresh) {
             FetchWeatherTask weatherTask = new FetchWeatherTask();
-            weatherTask.execute("94043");
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String location = prefs.getString(getString(R.string.pref_location_key),
+                    getString(R.string.pref_location_default));
+            weatherTask.execute(location);
             return true;
         }
         return super.onOptionsItemSelected(item);
