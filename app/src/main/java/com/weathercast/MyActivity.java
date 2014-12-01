6cb7818ce202bdae016b2de517a75f508a137e78
winstongit <winstongit@gmail.com>
2014-12-01 09:56:33 +0800

    v4.08-update-and-delete

--- app/src/main/java/com/weathercast/MyActivity.java
+++ app/src/main/java/com/weathercast/MyActivity.java
@@ -6,7 +6,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -73,32 +72,5 @@ public class MyActivity extends ActionBarActivity {
         } else {
             Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed!");
         }
-    }
-    @Override
-    protected void onResume() {
-        Log.v(LOG_TAG, "in onResume");
-        super.onResume();
-        // The activity has become visible (it is now "resumed").
-    }
-
-    @Override
-    protected void onPause() {
-        Log.v(LOG_TAG, "in onPause");
-        super.onPause();
-        // Another activity is taking focus (this activity is about to be "paused").
-    }
-
-    @Override
-    protected void onStop() {
-        Log.v(LOG_TAG, "in onStop");
-        super.onStop();
-        // The activity is no longer visible (it is now "stopped")
-    }
-
-    @Override
-    protected void onDestroy() {
-        Log.v(LOG_TAG, "in onDestroy");
-        super.onDestroy();
-        // The activity is about to be destroyed.
     }
 }
